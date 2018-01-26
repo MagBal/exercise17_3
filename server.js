0618be8ed636539cf586f1c75f2b9f402f3bacc4
Magda Balun <magi@op.pl>
2018-01-26 12:55:52 +0100

    Change server.js

--- server.js
+++ server.js
@@ -1,27 +1,26 @@
-var express = require('express');
-var fs = require('fs');
+var express = require("express");
+var fs = require("fs");
 var app = express();
-var stringifyFile = '';
-var bodyParser = require('body-parser');
+var stringifyFile = "";
+var bodyParser = require("body-parser");
 
 app.use(bodyParser.json());
 
-app.get('/getNote', function (req, res) {
-    fs.readFile('./test.json', 'utf8', function(err, data) {
-    	if (err) throw err;
-    	stringifyFile = data;
-    	res.send(data);
+app.get("/getNote", function(req, res) {
+	fs.readFile("./test.json", "utf8", function(err, data) {
+		if (err) throw err;
+		stringifyFile = data;
+		res.send(data);
 	});
 });
 
-app.post('/updateNote/:note', function (req, res) {
-    stringifyFile = stringifyFile + req.params.note;
-    fs.writeFile('./test.json', stringifyFile, function(err) {
-    	if (err) throw err;
-    	console.log('File updated');
-    	res.send(stringifyFile);
+app.post("/updateNote/:note", function(req, res) {
+	stringifyFile += req.params.note;
+	fs.writeFile("./test.json", stringifyFile, function(err) {
+		if (err) throw err;
+		console.log("File updated");
+		res.send(stringifyFile);
 	});
 });
 
 app.listen(3000);
-
